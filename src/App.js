8e3e60d574ba50e278adc4e0b7f74421dbe41ec7
Chris Skeen <chrisskeen2006@gmail.com>
2024-10-09 13:33:38 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// e3dcc849-cf03-4cc2-9016-7bfba82e149a
+
 import { useState, useEffect} from "react";
 import "./App.css"
 import data from "./accounts.json"
@@ -8,6 +10,12 @@ function App() {
   const playBtn = <button className="playBtn" onClick={() =>{
     setPageState("play")
   }}>Play</button>
+  const url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=e3dcc849-cf03-4cc2-9016-7bfba82e149a'
+  fetch(url)
+  .then(response => response.json)
+  .then(data => console.log(data))
+
+
 // -----------------------------------------------------------------------------------------------------------------------
 
 
@@ -16,7 +24,7 @@ function App() {
       <div className="App">
           <div className="main-title">Word Whiz</div>
           <div className="project-by-cont"><h2 className="project-by">Project By:</h2></div>
-          <div className="names-cont"><h4 className="names">Chris Skeen,</h4></div>
+          <div className="names-cont"><h4 className="names">Chris Skeen, </h4></div>
           <div className="play-btn-cont">{playBtn}</div>
       </div>
   
