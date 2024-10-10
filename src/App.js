c7960c245c5e909274dee733e697db22fe1c8d62
Chris Skeen <chrisskeen2006@gmail.com>
2024-10-10 10:12:44 -0500

    added api completely working + new animations for definitions and cool button features

--- src/App.js
+++ src/App.js
@@ -1,63 +1,78 @@
 // e3dcc849-cf03-4cc2-9016-7bfba82e149a
 
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./App.css"
-
 const url = 'https://random-word-api.herokuapp.com/word'
-fetch(url)
-.then(response => response.json())
-.then(data => findWordStuff(data[0]))
-let condition = true;
+const startBtn = <button onClick={apiWork} className="start-btn">Get Word</button>;
 
-function findWordStuff(data) {
-  while (condition == true) {
+function apiWork() {
+  function trying(data) {
     try {
-      console.log(data)
-      let newUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${data}`;
-      fetch(newUrl)
-      .then(response => response.json())
-      .then(data => console.log(data[0].meanings[0].definitions[0].definition))
-      condition = false;
-    } catch(err){
-      console.log(err)
-    }
+      console.log(data[0].meanings[0].definitions[0].definition)
+      console.log(document.getElementById('definition-area-cont').innerHTML = `<p class="definition-area">${data[0].meanings[0].definitions[0].definition}</p>`)
+      
 
+
+    } catch (err) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => findWordStuff(data[0]))
+    }
   }
+  
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => findWordStuff(data[0]))
+  
+  function findWordStuff(data) {
+    console.log(data)
+    let newUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${data}`;
+    fetch(newUrl)
+      .then(response => response.json())
+      .then(data => trying(data))
+  }
+
 }
+
+
 
 function App() {
   // BEFORE AREA --------------------------------------------------------------------------------------------------------
   const [pageState, setPageState] = useState("default");
-  const playBtn = <button className="playBtn" onClick={() =>{
+  const playBtn = <button className="playBtn" onClick={() => {
     setPageState("play")
   }}>Play</button>
-  
 
 
-// -----------------------------------------------------------------------------------------------------------------------
+
+  // -----------------------------------------------------------------------------------------------------------------------
 
 
   if (pageState === "default") {
     return (
       <div className="App">
-          <div className="main-title">Word Whiz</div>
-          <div className="project-by-cont"><h2 className="project-by">Project By:</h2></div>
-          <div className="names-cont"><h4 className="names">Chris Skeen, </h4></div>
-          <div className="play-btn-cont">{playBtn}</div>
+        <div className="main-title">Word Whiz</div>
+        <div className="project-by-cont"><h2 className="project-by">Project By:</h2></div>
+        <div className="names-cont"><h4 className="names">Chris Skeen, </h4></div>
+        <div className="play-btn-cont">{playBtn}</div>
       </div>
-  
+
     );
 
 
 
 
-  } else if (pageState === 'play'){
+  } else if (pageState === 'play') {
     return (
-    <div className="App">
-      <div className="main-title-play">Word Whiz</div>
-
-    </div>
-  );
+      <div className="App">
+        <div className="main-title-play">Word Whiz</div>
+        <div className="start-btn-cont">
+          {startBtn}
+        </div>
+        <div id="definition-area-cont" className="definition-area-cont"><p className="definition-area">This is where the definition will go!</p></div>
+      </div>
+    );
 
   }
 }
