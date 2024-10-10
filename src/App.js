9e185813160d2688528e457ad99fac0a306331fd
Kilan Miller <kmiller25@basecampcodingacademy.org>
2024-10-10 10:13:37 -0500

    my changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="App">
           <div className="main-title">Word Whiz</div>
           <div className="project-by-cont"><h2 className="project-by">Project By:</h2></div>
-          <div className="names-cont"><h4 className="names">Chris Skeen,</h4></div>
+          <div className="names-cont"><h4 className="names">Chris Skeen, Kilan Miller</h4></div>
           <div className="play-btn-cont">{playBtn}</div>
       </div>
   
@@ -37,3 +37,4 @@ function App() {
 }
 
 export default App;
+
